f2aa23b532f8e57584fabed5ac9d6b565d6c4e5f
ramlokesh59 <ramlokesh59@gmail.com>
2026-03-16 14:26:29 +0530

    Update PalindromeCheckerApp.java

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,8 +1,24 @@
+public class PalindromeCheck {
 
-public class PalindromeCheckerApp {
     public static void main(String[] args) {
-System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version 1.0");
-        System.out.println("System initialized Successfully");
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check if palindrome
+        if (word.equals(reversed)) {
+            System.out.println("The string \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
+        }
+
+        // Program ends
     }
 }
